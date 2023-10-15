7a270f5307c56802a9baad4acd8377729780825b
aaronjsharp <aaron.sharp2011@gmail.com>
2023-10-15 12:27:18 -0500

    added logout and account display in sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,6 +6,7 @@ import { usePathname } from 'next/navigation';
 import path from 'path';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
+import { UserItem } from './user-item';
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -113,7 +114,7 @@ export const Navigation = () => {
           <ChevronsLeft className='h-6 w-6' />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className='mt-4'>
           <p>Documents</p>
