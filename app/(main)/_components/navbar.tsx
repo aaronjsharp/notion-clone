35a8ff204b5e28db5c47b61323c4dc3c16880f0c
aaronjsharp <aaron.sharp2011@gmail.com>
2023-10-16 15:05:24 -0500

    banner to show when documents are in the trash

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "convex/react"
 import { useParams } from "next/navigation";
 import { MenuIcon } from "lucide-react"
 import { Title } from "./title";
+import { Banner } from "./banner";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -48,6 +49,9 @@ export const Navbar = ({
           <Title initialData={document} />
         </div>
       </nav>
+      {document.isArchived && (
+        <Banner documentId={document._id} />
+      )}
     </>
   )
 }
