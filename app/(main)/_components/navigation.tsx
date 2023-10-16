bc9d9672f267a12788fe9d29b4b279e0251089de
aaronjsharp <aaron.sharp2011@gmail.com>
2023-10-16 11:32:27 -0500

    settings dark mode modal

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -28,9 +28,11 @@ import { useSearch } from '@/hooks/use-search';
 import { Item } from './item';
 import { DocumentList } from './document-list';
 import { Trashbox } from './trash-box';
+import { useSettings } from '@/hooks/use-settings';
 
 export const Navigation = () => {
   const search = useSearch()
+  const settings = useSettings()
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
   const create = useMutation(api.documents.create);
@@ -146,7 +148,7 @@ export const Navigation = () => {
         <div>
           <UserItem />
           <Item label='Search' icon={Search} isSearch onClick={search.onOpen} />
-          <Item label='Settings' icon={Settings} onClick={() => {}} />
+          <Item label='Settings' icon={Settings} onClick={settings.onOpen} />
           <Item onClick={handleCreate} label='New page' icon={PlusCircle} />
         </div>
         <div className='mt-4'>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ConvexClientProvider } from '@/components/providers/convex-provider';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { ModalProvider } from '@/components/providers/modal-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -43,6 +44,7 @@ export default function RootLayout({
             storageKey='notion-theme-2'
           >
             <Toaster position="bottom-center" />
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
