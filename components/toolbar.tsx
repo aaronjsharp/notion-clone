70be7920abfe9901b37a453f8d9133fc76a314e3
aaronjsharp <aaron.sharp2011@gmail.com>
2023-10-17 13:16:38 -0500

    modal to upload image

--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -8,6 +8,7 @@ import { IconPicker } from './icon-picker';
 import { Button } from './ui/button';
 import { ElementRef, useRef, useState } from 'react';
 import { api } from '@/convex/_generated/api';
+import { useCoverImage } from "@/hooks/use-cover-image";
 
 interface ToolbarProps {
   initialData: Doc<'documents'>;
@@ -21,6 +22,7 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
 
   const update = useMutation(api.documents.update)
   const removeIcon = useMutation(api.documents.removeIcon)
+  const coverImage = useCoverImage()
 
   const enableInput = () => {
     if (preview) return 
@@ -101,7 +103,7 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
         )}
         {!initialData.coverImage && !preview && (
           <Button
-            onClick={() => {}}
+            onClick={coverImage.onOpen}
             className='text-muted-foreground text-xs'
             variant='outline'
             size='sm'
